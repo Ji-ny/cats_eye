a6fc7d48c0499507b8e349e40bcf1a7545608beb
Jiny <moc24@naver.com>
2024-05-19 22:25:40 +0900

    feat : 메인, 로그인 화면, 폴더 구조 생성, 반응형 기반

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import './App.scss';
+import { useEffect } from "react";
+import MainPage from "./pages/MainPage/MainPage";
 function App() {
+
+      // ** =============뷰포트 높이 구하기 ===============** //
+    
+      /** 현재 뷰포트 높이의 1%를 계산한다. */
+      const setVh = () => {
+        const vh = window.innerHeight * 0.01; //window.innerHeight : 뷰포트의 높이를 가져온다. | 뷰포트 높이를 100분의 1로 나누어서 사용하는 것 (1%)
+        document.documentElement.style.setProperty('--vh', `${vh}px`); // css 변수를 만들어준다.
+      }
+      // * 첫 렌더링시, 뷰포트 사이즈 계산 후 적용
+      useEffect(()=> {
+        setVh();
+        
+        // 사이즈가 변경될 때, 다시 뷰포트 높이를 구한다.
+        function onResize(){
+          setVh();
+        }
+                                // 이벤트 유형 , 이벤트가 발생할때 실행하는 함수.
+        window.addEventListener('resize', onResize);
+      },[]);
+
+
+    // ** ============================================** //
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='screen'>
+      <Routes>
+        <Route path = "*" element= {<div> 404 페이지입니다.</div>}></Route>
+        <Route path="/" element={<MainPage/>}/>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
     </div>
+
   );
 }
 
---
+++ src/styles/styles_custom.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+
+
+// 캣츠아이 : 핑크색 타이틀
+export const  TitlePink = styled.div`
+    box-sizing: border-box;
+    padding: 10px;
+
+    font-size: 2.5rem; // 25px
+    font-weight: bold;
+    color: #FFA6A6;
+    text-align: center;
+    line-height: 2rem;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 150px;
+    height: 15%;
+
+
+
+`;
+
+
+
